1921a9205adefb4418df05b8bac74837c3be1fc6
dasha <dasha.nikitina@auth0.com>
2021-05-27 14:15:21 -0600

    make it better

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const path = require("path")
 
 const PORT = process.env.PORT || 3001
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, 'client/build')));
 
 app.get("/api", (req, res) => {
   res.json({message: "Connected"})
 })
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
