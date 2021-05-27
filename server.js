d801d6849c3aaeca8744d9b3cd64a8e466b035e4
dasha <dasha.nikitina@auth0.com>
2021-05-26 21:50:39 -0600

    Deploy app to Heroku

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
 const PORT = process.env.PORT || 3001
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
   res.json({message: "Connected"})
 })
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
